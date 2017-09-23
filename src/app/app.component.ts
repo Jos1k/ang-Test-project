08b39a1f0dfa796355eca7cba2c7f76e7c002e7e
Ihor Hadzera <ihor.hadzera@additiv.ch>
2017-09-23 13:31:36 +0200

    added routings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { Hero } from './hero';
-import { HeroService } from './hero.service';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl:'./app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers:[ HeroService ]
+    selector: 'app-root',
+    template:`
+    <h1>{{title}}</h1>
+    <nav>
+        <a routerLink="/dashboard" routerLinkActive="active">Dasboard</a>
+        <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>`,
+    styleUrls:['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  title = 'Tour Of Heroes';
-  selectedHero:Hero;
-  heroes:Hero[];
-
-  constructor(private heroService:HeroService){}
-  
-  ngOnInit(): void {
-    this.getHeroes();
-  }
-  
-  getHeroes():void{
-    this.heroService.getHeroes().then(heroes=>this.heroes = heroes);
-  }
-
-  onSelect(hero:Hero):void {
-    this.selectedHero = hero;
-  }
-  
+export class AppComponent{
+    title = 'Tour Of Heroes';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
   ],
-  imports: [
-    BrowserModule,FormsModule
-  ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
